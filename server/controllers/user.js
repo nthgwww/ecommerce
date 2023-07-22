140ca3cb8db7544c46b18daa3caa49fd28a10642
ngocthong <6051071118@st.utc2.edu.vn>
2023-07-22 15:40:59 +0700

    #3 login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,15 +6,43 @@ const register = asyncHandler(async(req, res)=>{
     if(!email || !password || !firstname || !lastname)
     return res.status(400).json({
         success:false,
-        mess:'Missing inputs'
+        mes:'Missing inputs'
     })
-    const response = await Users.create(req.body)
-    return res.status(200).json({
-        success: response ? true : false,
-        response
+
+    const user = await User.findOne({email})
+    if(user)
+        throw new Error('User has existed!')
+    else{
+        const newUser = await User.create(req.body)
+        return res.status(200).json({
+            success: newUser ? true : false,
+            mes: newUser ? 'Register is successfully. Please go login ~' : 'Something went wrong!'
+        })
+    }
+})
+
+const login = asyncHandler(async(req, res)=>{
+    const {email, password} = req.body
+    if(!email || !password)
+    return res.status(400).json({
+        success:false,
+        mes:'Missing inputs'
     })
+
+    //plain object
+    const response = await User.findOne({email})
+    if(response && await response.isCorrectPassword(password)){
+        const {password, role, ...userData} = response.toObject()
+        return res.status(200).json({
+            success:true,
+            userData: response
+        })
+    }else{
+        throw new Error('Invalid credentials')
+    }
 })
 
 module.exports = {
-    register 
+    register,
+    login
 }
